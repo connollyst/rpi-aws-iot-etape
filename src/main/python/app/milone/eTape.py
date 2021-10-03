34287d7828048c80c929f1b9d11a5a3c707441e4
Sean Connolly <connolly.st@gmail.com>
2021-10-03 13:20:47 -1000

    Removed sample averaging (values still drift)

--- src/main/python/app/milone/eTape.py
+++ src/main/python/app/milone/eTape.py
@@ -27,19 +27,8 @@ class eTape:
         # adc = Adafruit_ADS1x15.ADS1015(address=0x49, busnum=1)
 
     def read(self):
-        self._value = self._average_samples(self._take_samples())
+        self._value = self._adc.read_adc(0, gain=self.GAIN)
         return self._value
-
-    def _take_samples(self, count=SAMPLE_COUNT):
-        start = time.time()
-        samples = [self._adc.read_adc(0, gain=self.GAIN) for _ in range(count)]
-        duration = time.time() - start
-        self._logger.info("Took {} samples in {}ms: {}".format(len(samples), duration, samples))
-        return samples
-
-    def _average_samples(self, samples, trim=EDGE_SAMPLE_COUNT):
-        readings = samples[trim:len(samples) - trim]
-        return sum(readings) / len(readings)
 
     def to_json(self):
         return {
